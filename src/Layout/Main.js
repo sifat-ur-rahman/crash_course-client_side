00bda0248e67544011cfe6088dbb56715385dd62
sifat-ur-rahman <md.sifat.ur.rahman2702@gmail.com>
2022-10-26 08:30:01 +0600

    main

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from '../Header/Header/Header';
+import LeftSideNav from '../Layout/Main'
 
 const Main = () => {
     return (
         <div>
-            
+            <Header></Header>
+            <Container>
+                <Row>
+                    <Col lg='3' >
+                        <LeftSideNav></LeftSideNav>
+                    </Col>
+                    <Col lg='9'>
+                        <Outlet></Outlet>
+                    </Col>
+                    
+                </Row>
+            </Container>
         </div>
     );
 };
---
+++ src/LeftSideNav/LeftSideNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftSideNav = () => {
+    return (
+        <div>
+           <h3>This is LeftSide Nav</h3> 
+        </div>
+    );
+};
+
+export default LeftSideNav;
