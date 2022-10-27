43dc8daae96779873efafa8b314cee9b2a7d1509
sifat-ur-rahman <md.sifat.ur.rahman2702@gmail.com>
2022-10-27 11:50:11 +0600

    end

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAqvdki5baAZ3EsHyafrwKN36a-9sXNzq8",
-  authDomain: "crash-course-eae91.firebaseapp.com",
-  projectId: "crash-course-eae91",
-  storageBucket: "crash-course-eae91.appspot.com",
-  messagingSenderId: "684198759937",
-  appId: "1:684198759937:web:a981ddff5ed7860514fa00"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
